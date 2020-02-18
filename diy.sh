265a498200c85268f57053abf752b0a8e7c45186
Ryan <yangchuansheng33@gmail.com>
2020-02-18 09:50:56 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -7,3 +7,7 @@
 #=================================================
 # Modify default IP
 sed -i 's/192.168.1.1/192.168.100.254/g' package/base-files/files/bin/config_generate
+mkdir package/my
+git clone https://github.com/Leo-Jo-My/luci-app-ssr-plus-Jo package/my/luci-app-ssr-plus-Jo
+./scripts/feeds update -a
+./scripts/feeds install -a
