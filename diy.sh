d2e24376cbc3c57a2de6f00e5e841aa1612a55a6
yangchuansheng <yangchuansheng33@gmail.com>
2020-02-19 16:26:28 +0000

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -8,6 +8,18 @@
 # Modify default IP
 sed -i 's/192.168.1.1/192.168.100.254/g' package/base-files/files/bin/config_generate
 mkdir package/my
-git clone https://github.com/Leo-Jo-My/luci-app-ssr-plus-Jo package/my/luci-app-ssr-plus-Jo
+echo "src-git lienol https://github.com/Lienol/openwrt-package" >> feeds.conf.default
+./scripts/feeds clean
 ./scripts/feeds update -a
+rm -rf feeds/lienol/package/ipt2socks
+rm -rf feeds/lienol/package/shadowsocksr-libev
+rm -rf feeds/lienol/package/pdnsd-alt
+rm -rf feeds/lienol/package/verysync
+rm -rf feeds/lienol/others/luci-app-verysync
+rm -rf package/lean/kcptun
+rm -rf package/lean/trojan
+rm -rf package/lean/luci-app-kodexplorer
+rm -rf package/lean/luci-app-pppoe-relay
+rm -rf package/lean/luci-app-pptp-server
+rm -rf package/lean/luci-app-v2ray-server
 ./scripts/feeds install -a
