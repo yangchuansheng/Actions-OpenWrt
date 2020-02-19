ae067603de98716a4ac01b56d1ca0ffa2fdbf7b6
yangchuansheng <yangchuansheng33@gmail.com>
2020-02-19 16:44:56 +0000

    add passwall workflow

---
+++ diy_passwall.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+#=================================================
+# Description: DIY script
+# Lisence: MIT
+# Author: P3TERX
+# Blog: https://p3terx.com
+#=================================================
+# Modify default IP
+sed -i 's/192.168.1.1/192.168.100.254/g' package/base-files/files/bin/config_generate
+mkdir package/my
+echo "src-git lienol https://github.com/Lienol/openwrt-package" >> feeds.conf.default
+./scripts/feeds clean
+./scripts/feeds update -a
+rm -rf feeds/lienol/package/ipt2socks
+rm -rf feeds/lienol/package/shadowsocksr-libev
+rm -rf feeds/lienol/package/pdnsd-alt
+rm -rf feeds/lienol/package/verysync
+rm -rf feeds/lienol/others/luci-app-verysync
+rm -rf package/lean/kcptun
+rm -rf package/lean/trojan
+rm -rf package/lean/luci-app-kodexplorer
+rm -rf package/lean/luci-app-pppoe-relay
+rm -rf package/lean/luci-app-pptp-server
+rm -rf package/lean/luci-app-v2ray-server
+./scripts/feeds install -a
