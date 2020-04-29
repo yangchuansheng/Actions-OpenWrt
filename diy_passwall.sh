d0256fd6a48e7fa44dd69624cc53166ae6195182
yangchuansheng <yangchuansheng33@gmail.com>
2020-04-29 03:42:02 +0000

    update diy_passwall.sh

--- diy_passwall.sh
+++ diy_passwall.sh
@@ -9,18 +9,13 @@
 sed -i 's/192.168.1.1/192.168.100.254/g' package/base-files/files/bin/config_generate
 sed -i "s/timezone='UTC'/timezone='CST-8'/" package/base-files/files/bin/config_generate
 sed -i "/timezone='CST-8'/a \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ set system.@system[-1].zonename='Asia/Shanghai'" package/base-files/files/bin/config_generate
-echo "src-git lienol https://github.com/Lienol/openwrt-package" >> feeds.conf.default
+echo "src-git small https://github.com/kenzok8/small" >> feeds.conf.default
+echo "src-git others https://github.com/kenzok8/openwrt-packages" >> feeds.conf.default
 ./scripts/feeds clean
 ./scripts/feeds update -a
-rm -rf feeds/lienol/package/ipt2socks
-rm -rf feeds/lienol/package/shadowsocksr-libev
-rm -rf feeds/lienol/package/pdnsd-alt
-rm -rf feeds/lienol/package/verysync
-rm -rf feeds/lienol/others/luci-app-verysync
-#rm -rf package/lean/kcptun
-#rm -rf package/lean/trojan
-rm -rf package/lean/luci-app-kodexplorer
-rm -rf package/lean/luci-app-pppoe-relay
-rm -rf package/lean/luci-app-pptp-server
-rm -rf package/lean/luci-app-v2ray-server
+rm -rf feeds/small/ipt2socks
+rm -rf feeds/small/pdnsd-alt
+rm -rf package/lean/kcptun
+rm -rf package/lean/trojan
+rm -rf feeds/small/v2ray-plugin
 ./scripts/feeds install -a
