dbe05d9178b5a24640cb79de3e98373514ec2665
Carson Yang <yangchuansheng33@gmail.com>
2022-01-13 11:59:24 +0800

    add dev

--- diy-part1.sh
+++ diy-part1.sh
@@ -14,48 +14,8 @@
 # Add a feed source
 #sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
 
-cat > package/base-files/files/etc/banner <<EOF
-        ´´´´´´´´██´´´´´´´
-        ´´´´´´´████´´´´´´
-        ´´´´´████████´´´´
-        ´´`´███▒▒▒▒███´´´´´
-        ´´´███▒●▒▒●▒██´´´
-        ´´´███▒▒▒▒▒▒██´´´´´
-        ´´´███▒▒▒▒██´                      apiVersion: v1
-        ´´██████▒▒███´´´´´                 kind: Router
-        ´██████▒▒▒▒███´´                   metadata:
-        ██████▒▒▒▒▒▒███´´´´                  name: openwrt
-        ´´▓▓▓▓▓▓▓▓▓▓▓▓▓▒´´                   labels:
-        ´´▒▒▒▒▓▓▓▓▓▓▓▓▓▒´´´´´                  kernel: 4.19
-        ´.▒▒▒´´▓▓▓▓▓▓▓▓▒´´´´´              spec:
-        ´.▒▒´´´´▓▓▓▓▓▓▓▒                     platform:
-        ..▒▒.´´´´▓▓▓▓▓▓▓▒                    - name: pve
-        ´▒▒▒▒▒▒▒▒▒▒▒▒                          resources:
-        ´´´´´´´´´███████´´´´´                    limits:
-        ´´´´´´´´████████´´´´´´´                    cpu: 2
-        ´´´´´´´█████████´´´´´´                     mem: 2Gi
-        ´´´´´´██████████´´´´             
-        ´´´´´´██████████´´´              大部分人都在关注你飞的高不高，却没人在乎你飞的累不累，这就是现实！
-        ´´´´´´´█████████´´                      我从不相信梦想，我，只，相，信，自，己！
-        ´´´´´´´█████████´´´
-        ´´´´´´´´████████´´´´´
-        ________▒▒▒▒▒
-        _________▒▒▒▒
-        _________▒▒▒▒
-        ________▒▒_▒▒
-        _______▒▒__▒▒
-        _____ ▒▒___▒▒
-        _____▒▒___▒▒
-        ____▒▒____▒▒
-        ___▒▒_____▒▒
-        ███____ ▒▒
-        ████____███
-        █ _███_ _█_███
-——————————————————————————女神保佑，代码无bug——————————————————————
-EOF
+cp $GITHUB_WORKSPACE/banner package/base-files/files/etc/banner
 
-# Modify default IP
-sed -i 's/192.168.1.1/192.168.100.254/g' package/base-files/files/bin/config_generate
 sed -i "s/timezone='UTC'/timezone='CST-8'/" package/base-files/files/bin/config_generate
 sed -i "/timezone='CST-8'/a \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ set system.@system[-1].zonename='Asia/Shanghai'" package/base-files/files/bin/config_generate
 
