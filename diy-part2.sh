3f090b6042ca6db4a9e9f12f0fde3d64edd58afa
Carson Yang <yangchuansheng33@gmail.com>
2022-01-13 15:51:48 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -21,3 +21,8 @@ sed -ri 's/luci-theme-\S+/luci-theme-ifit/g' feeds/luci/collections/luci/Makefil
 sed -i "s/sys.hostname()/\"WEEXIANG\"/g" feeds/others/luci-theme-ifit/files/templates/header.htm
 sed -i "/Powered by/ c\\ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ BAILIAN data ltd" feeds/others/luci-theme-ifit/files/templates/footer.htm
 sed -i "/ver.distversion/d" feeds/others/luci-theme-ifit/files/templates/footer.htm
+
+# netmaker
+sed -i "/^PKG_VERSION/ c\PKG_VERSION:=0.9.3" feeds/netmaker/netmaker/Makefile
+sed -i "/^PKG_SOURCE_VERSION/ c\PKG_SOURCE_VERSION:=106047243720605df6757ce706690a434d976c1a" feeds/netmaker/netmaker/Makefile
+sed -i "/^PKG_SOURCE_DATE/ c\PKG_SOURCE_DATE:=20220107" feeds/netmaker/netmaker/Makefile
